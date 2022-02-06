81b8fa4d7e1f8d37b5f7a1da863b8075953265bb
LaurentLienhard <laurent.lienhard@outlook.com>
2022-02-06 15:00:28 +0100

    update

--- WSLFusionSuite.sh
+++ WSLFusionSuite.sh
@@ -6,3 +6,17 @@ mkdir -p /var/www/fusionsuite
 
 git clone https://github.com/fusionSuite/backend.git  /var/www/fusionsuite/backend
 git clone https://github.com/fusionSuite/frontend.git /var/www/fusionsuite/frontend
+
+rm /etc/nginx/sites-enabled/default
+service mariadb start
+# Allow SSH Root Login
+sed -i 's|^#PermitRootLogin.*|PermitRootLogin yes|g' /etc/ssh/sshd_config
+echo "root:root123" | chpasswd
+
+cp ./Sources/fusionsuite.conf /etc/nginx/sites-enabled/fusionsuite.conf
+cp ./Sources/phinx.php /var/www/fusionsuite/backend
+
+service php7.4-fpm start
+service nginx start
+
+mysql -uroot -proot123 < ./Sources/configure-mariadb.sql
